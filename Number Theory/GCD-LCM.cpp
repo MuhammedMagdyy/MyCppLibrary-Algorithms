fe55236de40c245de27440d0535199706c0be21d
MuhammedMagdyy <mohamedmody04@gmail.com>
2021-05-08 06:08:08 +0200

    Create GCD-LCM.cpp

---
+++ Number Theory/GCD-LCM.cpp
@@ -0,0 +1,11 @@
+//Function To Get GCD (Greatest Common Divisor)
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+//Function To Get LCM (Least Common Multiple)
+int lcm(int a, int b) {
+    return (a / gcd(a, b) * b);
+}
