3bfca85e7116a0265d822d8cc8fea74686e8e5dc
MuhammedMagdyy <mohamedmody04@gmail.com>
2021-05-08 06:41:36 +0200

    Delete DecimalTobinary.cpp

--- General Algorithms/DecimalTobinary.cpp
+++
@@ -1,9 +0,0 @@
-//Function To Convert From Decimal To Binary
-string Dec_To_Bin(int n) {
-    string s;
-    while (n) {
-        s += (char) ((n % 2) + '0'), n /= 2;
-    }
-    reverse(s.begin(), s.end());
-    return s;
-}
